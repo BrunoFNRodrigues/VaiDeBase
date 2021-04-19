3cea9f9355cde4d6eb945ecc85ee9eba8118735d
BrunoFNRodrigues <bf17669@gmail.com>
2021-04-19 15:42:18 -0300

    Adiciona classe Usuario

--- src/br/edu/insper/desagil/vaiDeBase/Usuario.java
+++ src/br/edu/insper/desagil/vaiDeBase/Usuario.java
@@ -1,5 +1,31 @@
 package br.edu.insper.desagil.vaiDeBase;
 
-public class Usuario {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Usuario {
+	private String nome;
+	private List<Video> videos;
+	
+	public Usuario(String nome) {
+		this.nome = nome;
+		this.videos = new ArrayList<>();
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public Video postaVideo(int id, Produto prod) {
+		return new Video(id, this, prod);
+	}
+	
+	public int totalAvaliacoes() {
+		int tot = 0;
+		for (Video video: this.videos) {
+			tot = tot + video.mediaAvaliacoes();
+		}
+		return tot;
+	}
+	
 }
