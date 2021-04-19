3d7a6e35526a12b7846734d355c0d0f740109f8e
BrunoFNRodrigues <bf17669@gmail.com>
2021-04-19 15:42:00 -0300

    adiociona classe Vendedor

--- src/br/edu/insper/desagil/vaiDeBase/Vendedor.java
+++ src/br/edu/insper/desagil/vaiDeBase/Vendedor.java
@@ -1,5 +1,17 @@
 package br.edu.insper.desagil.vaiDeBase;
 
-public class Vendedor {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Vendedor extends Usuario{
+	private List<Produto> produtos;
+	
+	public Vendedor(String nome) {
+		super(nome);
+		this.produtos = new ArrayList<>();
+	}
+	
+	public void cadastraProduto(String nome, double preco) {
+		produtos.add(new Produto(nome, preco));
+	}
 }
