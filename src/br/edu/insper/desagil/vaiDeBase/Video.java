11adcf8c31116557a5edabd73b59a616cecec8a3
BrunoFNRodrigues <bf17669@gmail.com>
2021-04-19 15:41:40 -0300

    Adiociona classe Video

--- src/br/edu/insper/desagil/vaiDeBase/Video.java
+++ src/br/edu/insper/desagil/vaiDeBase/Video.java
@@ -1,5 +1,47 @@
 package br.edu.insper.desagil.vaiDeBase;
 
-public class Video {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Video {
+	private int id;
+	private Usuario usuario;
+	private Produto produto;
+	private Map<Usuario,Integer> avaliacoes;
+	
+	public Video(int id, Usuario usuario, Produto produto) {
+		super();
+		this.id = id;
+		this.usuario = usuario;
+		this.produto = produto;
+		this.avaliacoes = new HashMap<>();
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+	
+	public void adiocionaAvalicao(Usuario nome, int avalicao) {
+		if (nome != this.usuario && avalicao > 1 && avalicao < 5) {
+			avaliacoes.put(nome, avalicao);
+		}
+	}
+	
+	public int mediaAvaliacoes() {
+		int tot = 0;
+		double media = 0.0;
+		for (Usuario nome:avaliacoes.keySet()) {
+			int nota = avaliacoes.get(nome);
+			tot = tot + nota;
+		}
+		if (avaliacoes.size() != 0) {
+			media = tot/avaliacoes.size();
+		}
+		return (int) Math.round(media);
+	}
 }
